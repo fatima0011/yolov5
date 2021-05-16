e3ab171d75eeb08e96c727b396223a7c02096e2c
fatima0011 <83467444+fatima0011@users.noreply.github.com>
2021-05-16 19:38:30 +0500

    Update detect.py

--- detect.py
+++ detect.py
@@ -31,7 +31,7 @@ def detect(save_img=False):
     device = select_device(opt.device)
     half = device.type != 'cpu'  # half precision only supported on CUDA
 
-    # Load model
+    # Load model attempt
     model = attempt_load(weights, map_location=device)  # load FP32 model
     stride = int(model.stride.max())  # model stride
     imgsz = check_img_size(imgsz, s=stride)  # check img_size
